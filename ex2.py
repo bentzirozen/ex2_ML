a65147c0a9a2811e2530cb511e756f461cff8a8c
Benzion Rozen <benzion.rozen@live.biu.ac.il>
2019-05-19 16:20:28 +0300

    finished

--- ex2.py
+++ ex2.py
@@ -1,10 +1,9 @@
-from numpy.random import shuffle
 import numpy as np
-
+import sys
 def perceptron(X,Y):
         z_score_norm(X)
-        eta = 1
-        iters = 18
+        eta = 0.1
+        iters = 15
         weights = np.zeros((3, X.shape[1]))
         set = np.c_[X, Y]
         for _ in range(iters):
@@ -19,9 +18,7 @@ def perceptron(X,Y):
                     y_hat = int(y_hat)
                     weights[y, :] += eta * x
                     weights[y_hat, :] += -eta * x
-        test_x = read_data("check_x.txt")
-        test_y = np.genfromtxt("check_y.txt",delimiter=',')
-        test(test_x,test_y,weights)
+        return weights
 
 def passive_agressive(X,Y):
     z_score_norm(X)
@@ -35,33 +32,40 @@ def passive_agressive(X,Y):
                 y = int(y)
                 y_hat = int(y_hat)
                 loss = max(0.0, 1 - np.dot(weights[y], x) + np.dot(weights[y_hat], x))
-                loss /= ((np.power(np.linalg.norm(x, ord=2), 2)) * 2)
-                weights[y, :] = weights[y, :] + loss * x
-                weights[y_hat, :] = weights[y_hat, :] - loss * x
-    test_x = read_data("check_x.txt")
-    test_y = np.genfromtxt("check_y.txt", delimiter=',')
-    test(test_x, test_y, weights)
+                norm = ((np.power(np.linalg.norm(x, ord=2), 2)) * 2)
+                if(norm!=0):
+                    loss/=norm
+                    weights[y, :] = weights[y, :] + loss * x
+                    weights[y_hat, :] = weights[y_hat, :] - loss * x
+    return weights
 
-def min_max_norm(examples):
-
-    features = len(examples[0])
-    new_max = 10
-    new_min = 4
-
-    for i in range(features):
-        old_min = np.amin(examples[:, i])              # minimum feature value .
-        denom = np.amax(examples[:, i]) - old_min      # old_max - old_min .
-        if denom!=0:
-            value = (examples[:, i] - old_min) / denom
-            examples[:, i] = value * (new_max - new_min) + new_min
+def svm(X,Y):
+    z_score_norm(X)
+    iters = 50
+    eta =0.01
+    alpha = 0.1
+    weights = np.zeros((3, X.shape[1]))
+    for i in range(iters):
+        for x, y in zip(X, Y):
+            y_hat = np.argmax(np.dot(weights, x))
+            # update
+            if y != y_hat:
+                y = int(y)
+                y_hat = int(y_hat)
+                weights[y, :] = (1 - eta * alpha) * weights[y, :] + eta * x
+                weights[y_hat, :] = (1 - eta * alpha) * weights[y_hat, :] - eta * x
+            for i in range(weights.shape[0]):
+                if i != y and i != y_hat:
+                    weights[i, :] = (1 - eta * alpha) * weights[i, :]
+    return weights
 
 def z_score_norm(examples):
     features = examples.shape[1]
     for i in range(features):
         mean = np.mean(examples[:, i])
-        std_dev = np.std(examples[:, i])
-        if std_dev !=0:
-            examples[:, i] = (examples[:, i] - mean) / std_dev
+        denom = np.std(examples[:, i])
+        if denom !=0:
+            examples[:, i] = (examples[:, i] - mean) / denom
 
 
 def read_data(data):
@@ -74,28 +78,28 @@ def read_data(data):
             X.append(np.array(idx, dtype=np.float64))
     return np.asarray(X)
 
-def test(test_examples, test_labels,weights):
-        errors = 0
+def test(test_examples, test_labels,pa_weights,per_weights,svm_weights):
         m = test_examples.shape[0]  # number of test examples
         z_score_norm(test_examples)
         for i in range(m):
             # y_hat = np.argmax(np.dot(self.weights, test_examples[i]))
-            y_hat = np.argmax(np.dot(weights, test_examples[i]))
+            y_hat = np.argmax(np.dot(per_weights, test_examples[i]))
+            per_result = str(y_hat)
+            per_result+=","
+            y_hat = np.argmax(np.dot(pa_weights, test_examples[i]))
+            pa_result = str(y_hat)
+            y_hat = np.argmax(np.dot(svm_weights, test_examples[i]))
+            svm_result = str(y_hat)
+            svm_result+=","
+            print("perceptron: ",per_result,"svm: ",svm_result,"pa: ",pa_result)
 
-            if test_labels[i] != y_hat:
-                errors = errors + 1
 
-        loss = float(errors) / m
-
-        print("number of errors: " + str(errors))
-        print("number of tests: " + str(m))
-        print("the Loss is: " + str(loss))
-        print("which means: " + str((1 - loss) * 100) + " of correctness")
-
-def main(args):
-    X = read_data(args[1])
-    Y = np.genfromtxt(args[2],delimiter=',')
-    passive_agressive(X,Y)
-    perceptron(X,Y)
 if __name__ == '__main__':
-    main(['ex2.py', 'train_x.txt', 'train_y.txt', 'test_x.txt'])
+    args = sys.argv
+    X = read_data(args[1])
+    Y = np.genfromtxt(args[2], delimiter=',')
+    pa_weights = passive_agressive(X, Y)
+    perceptron_weights = perceptron(X, Y)
+    svm_weights = svm(X, Y)
+    test_x = read_data(args[3])
+    test(test_x, Y, pa_weights, perceptron_weights, svm_weights)
